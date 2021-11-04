0944b105b3b68e4a3766b01879e24a428ab37c07
MoisesNunez435 <moidavnused@gmail.com>
2021-11-03 19:00:05 -0600

    Agregar archivos de proyecto.

---
+++ Domain/Registro.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Registro
+    {
+        public EscalaTemperatura escalaTemperatura { get; set; }
+        public decimal GradosCelcius { get; set; }
+        public decimal Kelvin { get; set; }
+        public decimal GradosFahrenheit { get; set; }
+    }
+}
